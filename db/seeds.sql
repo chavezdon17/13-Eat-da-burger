103264f389235102323958ce9ec18414b8afce39
Donovan Chavez <chavezdon17@gmail.com>
2020-12-14 23:50:47 -0600

    added burgers db

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db; 
+
+INSERT INTO burger (name) VALUES ('Cheeseburger');
+INSERT INTO burger (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burger (name) VALUES ('Impossible burger');
