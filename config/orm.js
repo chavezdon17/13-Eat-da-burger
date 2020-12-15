747c1d4db605d7aac4a61f36f0996bd541d0d4f0
Donovan Chavez <chavezdon17@gmail.com>
2020-12-15 00:40:58 -0600

    updated orm

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,4 @@
+const { query } = require("../config/connection.js");
 const connection = require("../config/connection.js");
 
 function printQuestions(num) {
@@ -19,3 +20,48 @@ function objToSql(ob) {
   }
   return arr.toString();
 }
+
+const orm = {
+  selectAll: (tableInput, cb) => {
+    let queryString = "SELECT * FROM" + tableInput + ";";
+    connection.query(queryString, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  insertOne: (table, cols, vals, cb) => {
+    let queryString = "INSERT INTO" + table;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += " )";
+    queryString += " VALUES (";
+    queryString += printQuestions(vals.length);
+    queryString += " )";
+    console.log(queryString);
+
+    connection.query(queryString, vals, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  updateOne: (table, objColVals, condition, cb) => {
+    let queryString = "UPDATE" + tables;
+
+    queryString += "SET";
+    queryString += objToSql(objToSql);
+    queryString += " WHERE";
+    queryString += condition;
+    connection.query(queryString, vals, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+};
+
+module.exports = orm;
